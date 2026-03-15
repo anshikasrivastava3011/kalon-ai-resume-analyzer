bbe03cc82623fd491ad37ee377a1b804b5c7aedb
anshikasrivastava3011 <anshika.srivas3800@gmail.com>
2026-03-15 18:16:14 +0530

    Changes

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import rateLimit from "express-rate-limit";
 import analyzeRoutes from "./routes/analyzeRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 
