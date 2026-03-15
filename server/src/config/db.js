343df42829080730a37cf3af5ba90edd64bf3a4e
anshikasrivastava3011 <anshika.srivas3800@gmail.com>
2026-03-15 18:45:16 +0530

    Changes

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -7,8 +7,12 @@ const { Pool } = pg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-pool.query("SELECT NOW()")
+pool
+  .query("SELECT NOW()")
   .then(() => console.log("✅ PostgreSQL connected successfully"))
-  .catch(err => console.error("❌ PostgreSQL connection error:", err));
+  .catch((err) => console.error("❌ PostgreSQL connection error:", err));
